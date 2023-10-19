c2eb6f72b7ea2a52c5db7ca5dce33d59973ea380
Dipika Shekhawat <dipika.shekhawat@bluealtair.com>
2023-10-19 16:54:24 +0530

    updated schema.lua

--- kong/plugins/test-xml-to-lua/handler.lua
+++ kong/plugins/test-xml-to-lua/handler.lua
@@ -52,7 +52,7 @@ function plugin:access(config)
     -- Convert the XML tree to a Lua table
     local lua_table = {}
     --kong.log.set_serialize_value("lua_table1", lua_table)
-    kong.log.set_serialize_value("handler.root", handler.root)
+    kong.log.set_serialize_value("handler.root", handler)
     lua_table = xml_tree_to_lua_table(handler.root)
     kong.log.set_serialize_value("lua_table2", json.encode(lua_table))
     kong.service.request.set_raw_body(json.encode(lua_table))
