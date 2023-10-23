9fdf131500a3d68214b7a2ee3a31c13fb44e4d97
Dipika Shekhawat <dipika.shekhawat@bluealtair.com>
2023-10-23 18:32:15 +0530

    updaed handler.lua

--- kong/plugins/test-xml-to-lua/handler.lua
+++ kong/plugins/test-xml-to-lua/handler.lua
@@ -57,37 +57,6 @@ function plugin:body_filter(config)
   -- Implement logic for the body_filter phase here (http)
   if config.enable_on_response then
     local initialResponse = kong.service.response.get_raw_body()
-    local xml = initialResponse
-    local handler = require("xmlhandler.tree")
-    handler = handler:new()
-    --Instantiates the XML parser
-    local parser = xml2lua.parser(handler)
-
-    parser:parse(xml)
-
-    -- Function to convert the XML tree to a Lua table recursively
-    local function xml_tree_to_lua_table(xml_tree)
-      local result = {}
-      for tag, value in pairs(xml_tree) do
-        if type(value) == "table" then
-          if #value == 1 and type(value[1]) == "string" then
-            -- Handle single-value elements
-            result[tag] = value[1]
-          else
-            -- Handle nested elements recursively
-            result[tag] = xml_tree_to_lua_table(value)
-          end
-        else
-          -- Handle attributes
-          result[tag] = value
-        end
-      end
-      return result
-    end
-
-    -- Convert the XML tree to a Lua table
-    local lua_table = xml_tree_to_lua_table(handler.root)
-    kong.service.request.set_raw_body(json.encode(lua_table))
   end
 end
 -- return our plugin object
